6df1a4fd48989ad5814dd30fc455d174299588d1
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-15 07:12:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function shortcut(s1, s2) {
-  // your code here
+  s1 = s1.trim();
+  s2 = s2.trim();
+  if(s1.length && s2.length) {
+    return s1[0]+s2[0];
+  }
+  else return '\'\'' ;
 }
 
 // Do not change the code below.
